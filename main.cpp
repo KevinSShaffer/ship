6ea9bcb768affc00a6e8191e2bac7a39ec394dd6
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-09-24 20:30:39 -0600

    ship position validation

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 
 #include <iostream>
 #include <string>
+#include <time.h>
+#include <stdlib.h>
 #include "main.h"
 #include "position.h"
 #include "submarine.h"
+#include "..\grid\grid.h"
 
 using namespace std;
 
 int main()
-{	
-	int x = getInput("Enter the x coordinate: "),
-		y = getInput("Enter the y coordinate: "),
-		length = getInput("Enter the length: "),
+{
+	srand(time(0));
+	int x, y, length, depth, grid_x = 10, grid_y = 10;
+	Position::Orientation orientation;
+
+	do
+	{
+		x = getInput("Enter the x coordinate: ");
+		y = getInput("Enter the y coordinate: ");
+		length = getInput("Enter the length: ");
 		depth = getInput("Enter the dive depth in meters: ");
-	Position::Orientation orientation = getOrientation();
+		orientation = getOrientation();
+	} while (!isValid(orientation, x, y, length, grid_x, grid_y));
 
 
 
+	cout << "Press a key to exit: ";
+	cin >> x;
 	return 0;
 }
 int getInput(string question)
@@ -36,15 +48,21 @@ int getInput(string question)
 }
 Position::Orientation getOrientation()
 {
-	char letter;
-
+	char letter = '\0';
 	do
 	{
 		cout << "Horizontal (h) or Verical (v) orientation: " << endl;
 		cin >> letter;
 		cin.clear();
 		cin.ignore(256, '\n');
-	} while (!letter || letter != 'h' || letter != 'H' || letter != 'v' || letter != 'V');
+	} while (!letter || letter != 'h' && letter != 'H' && letter != 'v' && letter != 'V');
 
 	return ((letter == 'h' || letter == 'H') ? Position::HORIZONTAL : Position::VERTICAL);
+}
+bool isValid(Position::Orientation orientation, int x, int y, int length, int max_x, int max_y)
+{
+	if (orientation == Position::HORIZONTAL)
+		return x + length <= max_x && y <= max_y;
+	else
+		return x <= max_x && y + length <= max_y;
 }
--- main.h
+++ main.h
@@ -4,3 +4,4 @@
 
 int getInput(std::string question);
 Position::Orientation getOrientation();
+bool isValid(Position::Orientation orientation, int x, int y, int length, int max_x, int max_y);
